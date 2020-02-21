b4c79b43e0b0ea087f3df8adf2891826f1eb4e16
TMo484 <39891580+TMo484@users.noreply.github.com>
2020-02-20 21:28:10 -0500

    added a recipe page, and back arrow functionality

--- src/components/headers/pageHeader.js
+++ src/components/headers/pageHeader.js
@@ -3,11 +3,17 @@ import NavigationButton from '../buttons/navigationButton';
 import { HOMEPAGE } from '../../redux/reducer';
 
 class PageHeader extends React.Component {
+
+    handleGoBack() {
+            window.history.back()
+    }
+
     render() {
         return (
             <React.Fragment>
                 <div className="header page_header horizontally_spaced">
                     <div className="header_section"></div>
+                    <i class="fas fa-arrow-left fa-3x" onClick={this.handleGoBack.bind(this)}></i>
                     <NavigationButton buttonType="header_button" route="/find" label="Recipe Search" target={ HOMEPAGE }/>
                     <NavigationButton buttonType="header_button" route="/planning" label="Meal Planning" target="LOGINPAGE"/>
                     <NavigationButton buttonType="header_button" route="/grocery" label="Grocery List" target="LOGINPAGE"/>
